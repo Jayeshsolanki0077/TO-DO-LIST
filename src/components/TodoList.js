1445cc54d685a75798c14904468c2af33880fb98
Jayeshsolanki0077 <jayeshsolanki0077@gmail.com>
2020-04-30 20:20:35 +0530

    TODOLISTAPP

---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends Component {
+    render() {
+        const {items,clearList,handleDelete,handleEdit} = this.props;
+        return (
+            <div className="List-group my-5">
+                <h3 className="text-capitalize text-center">
+                    TodoList
+                </h3>
+                {
+                    items.map(item => {
+                        return <TodoItem key={item.id} title={item.title} handleDelete={ () => handleDelete(item.id)}
+                                                                            handleEdit ={ () =>handleEdit(item.id) }/>
+                    })
+                }
+                
+
+                <button type="submit" onClick={clearList} className="btn btn-danger btn-block 
+                                    text-capitalize mt-5"> Clear </button>                
+                  
+            </div>
+        )
+    }
+}
